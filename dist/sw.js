3be37e902de47f21ed155e5cf6d5c77f116a382d
Anthony David Rodgers <a.d.rodgers@bham.ac.uk>
2023-01-18 11:30:25 +0000

    Disable cache

--- dist/sw.js
+++ dist/sw.js
@@ -1,25 +1,25 @@
-var cacheName = 'gravity_model_webapp_pwa';
-var filesToCache = [
-  './',
-  // './index.html#dev',
-  './gravity_model_webapp.js',
-  './gravity_model_webapp_bg.wasm',
-];
+// var cacheName = 'gravity_model_webapp_pwa';
+// var filesToCache = [
+//   './',
+//   './index.html',
+//   './gravity_model_webapp.js',
+//   './gravity_model_webapp_bg.wasm',
+// ];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-});
+// /* Start the service worker and cache all of the app's content */
+// self.addEventListener('install', function (e) {
+//   e.waitUntil(
+//     caches.open(cacheName).then(function (cache) {
+//       return cache.addAll(filesToCache);
+//     })
+//   );
+// });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+// /* Serve cached content when offline */
+// self.addEventListener('fetch', function (e) {
+//   e.respondWith(
+//     caches.match(e.request).then(function (response) {
+//       return response || fetch(e.request);
+//     })
+//   );
+// });
